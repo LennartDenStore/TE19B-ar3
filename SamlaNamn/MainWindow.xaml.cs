600800cfa0fb95f05172a42c5a276b21c55d0e42
LennartDenStore <70271050+LennartDenStore@users.noreply.github.com>
2021-10-04 13:21:13 +0200

    Träna på listor

---
+++ SamlaNamn/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SamlaNamn
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        static List<string> namnlista = new List<string>();
+
+        static int antal = 0;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void ClickLäggTill(object sender, RoutedEventArgs e)
+        {
+            // Läs av namnet
+            string namn = rutaNamn.Text;
+
+            // Om namnet ej tomt
+            if (namn != "")
+            {
+                rutaNamn.Text = "";
+                rutaLista.Text += namn + "\n";
+                rutaFel.Text = "Inga fel...";
+                namnlista.Add(namn);
+            }
+            else
+            {
+                rutaFel.Text = "Inget namn matades in, försök igen!";
+            }
+        }
+        private void ClickSpara(object sender, RoutedEventArgs e)
+        {
+            File.WriteAllLines(@"namnlista.txt", namnlista);
+            rutaFel.Text = "Namnen sparade i namnlista.txt";
+        }
+    }
+}
