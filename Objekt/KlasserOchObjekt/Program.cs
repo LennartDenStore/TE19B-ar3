c36f5e1f6dd42e7847656d5f389d700c86fac822
LennartDenStore <70271050+LennartDenStore@users.noreply.github.com>
2022-01-10 13:01:14 +0100

    box

---
+++ Objekt/KlasserOchObjekt/Program.cs
@@ -0,0 +1,63 @@
+﻿using System.ComponentModel;
+using System;
+
+namespace KlasserOchObjekt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int num = 0;
+
+            // Create objekt of class type Box
+            Box box1 = new Box();
+            Box box2 = new Box();
+
+            Console.WriteLine("<skriv en längd för box 1: ");
+            double n1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("<skriv ett djup för box 1: ");
+            double n2 = double.Parse(Console.ReadLine());
+            Console.WriteLine("<skriv en höjd för box 1: ");
+            double n3 = double.Parse(Console.ReadLine());
+
+            box1.Length = n1;
+            box1.Breadth = n2;
+            box1.Hight = n3;
+            double Volume = box1.getVolume();
+            double Area = box1.getArea();
+
+            Console.WriteLine("<skriv en längd för box 2: ");
+            n1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("<skriv ett Djup för box 2: ");
+            n2 = double.Parse(Console.ReadLine());
+            Console.WriteLine("<skriv en höjd för box 2: ");
+            n3 = double.Parse(Console.ReadLine());
+
+            box2.Length = n1;
+            box2.Breadth = n2;
+            box2.Hight = n3;
+
+            Console.WriteLine($"Box 1 Dimensions Are: {box1.Length},{box1.Breadth},{box1.Hight}");
+            Console.WriteLine($"Box 1 volume is: {Volume}");
+            Console.WriteLine($"Box 1 area is: {Area}");
+
+            Console.WriteLine($"Box 2 Dimensions Are: {box2.Length},{box2.Breadth},{box2.Hight}");
+            Console.WriteLine($"Box 2 volume is: {box2.getVolume()}");
+            Console.WriteLine($"Box 2 area is: {box2.getArea()}");
+        }
+    }
+    class Box
+    {
+        public double Length { get; set; }
+        public double Breadth { get; set; }
+        public double Hight { get; set; }
+        public double getVolume()
+        {
+            return Length * Breadth * Hight;
+        }
+        public double getArea()
+        {
+            return Length * Breadth;
+        }
+    }
+}
---
+++ Objekt/Räkna timlön med klasser/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Räkna_timlön_med_klasser
+{
+    public class PersonInkomst
+    {
+        public string namn;
+        public int inkomst;
+        public int timmar;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Skapar objektet person1 från klassen PersonInkomst
+            PersonInkomst person1 = new PersonInkomst();
+            
+             Console.Write("Ange ditt namn: ");
+            string namn1 = Console.ReadLine();
+            System.Console.Write("Ange din inkomst: ");
+            int inkomst1 = int.Parse(Console.ReadLine());
+            Console.Write("Ange antal timmar: ");
+            int timmar1 = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Din timlön blev {inkomst1 / timmar1} kr/h");
+
+            // Skapa ett till objekt person2
+            PersonInkomst person2 = new PersonInkomst();
+            Console.Write("Ange ditt namn: ");
+            string namn2 = Console.ReadLine();
+            System.Console.Write("Ange din inkomst: ");
+            int inkomst2 = int.Parse(Console.ReadLine());
+            Console.Write("Ange antal timmar: ");
+            int timmar2 = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Din timlön blev {inkomst2 / timmar2} kr/h");
+
+            // Skapa ett till objekt person2
+            PersonInkomst person3 = new PersonInkomst();
+            Console.Write("Ange ditt namn: ");
+            string namn3 = Console.ReadLine();
+            System.Console.Write("Ange din inkomst: ");
+            int inkomst3 = int.Parse(Console.ReadLine());
+            Console.Write("Ange antal timmar: ");
+            int timmar3 = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Din timlön blev {inkomst3 / timmar3} kr/h");
+        }
+    }
+}
